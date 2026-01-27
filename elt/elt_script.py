070495e944b7ca595dd1ae686f6d17f97bcf2bca
Jake Korobellis <jkorobellis@gmail.com>
2026-01-27 12:23:10 -0500

    Socurce and dest configs

--- elt/elt_script.py
+++ elt/elt_script.py
@@ -16,3 +16,22 @@ def wait_for_postgres(host, max_retires=5, delay=5):
             time.sleep(delay)
     print("Max retries reached. PostgreSQL is not ready.")
     return False
+
+if not wait_for_postgres(host="source_postgres"):
+    exit(1)
+
+print("Starting ETL process...")
+
+source_config = {
+    'dbname': 'source_db',
+    'user': 'postgres',
+    'password': 'secret',
+    'host': 'source_postgres'
+}
+
+destination_config = {
+    'dbname': 'destination_db',
+    'user': 'postgres',
+    'password': 'secret',
+    'host': 'destination_postgres'
+}   
